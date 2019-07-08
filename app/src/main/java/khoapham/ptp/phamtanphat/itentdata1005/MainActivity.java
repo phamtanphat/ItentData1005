cdda5321c92860cb020f8cf56017d674268ce04e
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-08 14:05:41 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/itentdata1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/itentdata1005/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,Main2Activity.class);
-                intent.putExtra("so",-123);
+                intent.putExtra("so",1);
                 startActivity(intent);
             }
         });
